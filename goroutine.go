61053a65898b1bcd0a3c49e21000547785697b36
Minhaj401 <minhajnoushad123@gmail.com>
2026-01-10 13:42:14 +0530

    .

---
+++ goroutine.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+import "time"
+func task(id int){
+	fmt.Println("doing task",id)
+}
+func main(){
+	for i:=0;i<=10;i++{
+		go task(i)
+	}
+	time.Sleep(time.Second * 2)
+}
